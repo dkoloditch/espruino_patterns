cd6356fbe4e0865903c25613d5129c4bc9399ce5
Derek Koloditch <derek.koloditch@gmail.com>
2017-10-28 01:09:01 +0000

    flicker - solid, consistent color rotation

--- code/flicker.js
+++ code/flicker.js
@@ -3,8 +3,9 @@ var cols = 8;
 var led_count = rows*cols;
 var grb = new Uint8ClampedArray(led_count * 3);
 var pos = 0;
-var min = 0;
+var min = 1;
 var max = 5;
+var standardColor = [];
 
 function getColor() {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -12,16 +13,17 @@ function getColor() {
 
 function getPattern() {
   pos++;
+  standardColor = [getColor(), getColor(), getColor()];
   for (var i = 0; i < grb.length; i += 3) {
-     grb[i  ] = getColor();
-     grb[i+1] = getColor();
-     grb[i+2] = getColor();
+     grb[i  ] = standardColor[0];
+     grb[i+1] = standardColor[1];
+     grb[i+2] = standardColor[2];
   }
   return grb;
 }
 
 setInterval(function() {
   require("neopixel").write(D32, getPattern());
-}, 1000);
+}, 500);
 
 save();
